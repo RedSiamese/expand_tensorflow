ad2bfa2bb88616fc52d04247f5275339a141401c
王翔宇 <wxyriddler@outlook.com>
2019-08-08 12:44:52 +0800

    新建

---
+++ unpooling.py
@@ -0,0 +1,10 @@
+import tensorflow as tf 
+
+def unpooling2d(x,strides=2,name='unpooling2d'):
+    with name_scope(name):
+        x_shape=x.get_shape().as_list()
+        x=tf.expand_dims(x,3)
+        x=tf.concat([x]*strides[1],3)
+        x=tf.concat([x]*strides[0],2)
+        x=tf.reshape(x,[-1,x_shape[1]*strides[0],x_shape[2]*strides[1],x_shape[3]])
+    return x
